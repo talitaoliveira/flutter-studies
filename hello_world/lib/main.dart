a97d2ae737de2de71ac45fc611b5854fc08351a9
Talita Oliveira <latalita.s@Talitas-MacBook-Pro.local>
2020-01-06 18:32:41 -0300

    Second excercise

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -6,28 +6,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        title: 'Hello World',
         home: Scaffold(
-            appBar: AppBar(title: Text('Ola mundo')),
-            body: ListView(
-              children: <Widget>[
-                Container(
-                  child: const Center(
-                      child: Text(
-                    'Olá',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 100.0),
-                  )),
-                ),
-                Icon(
-                  Icons.favorite,
-                  color: Colors.pink,
-                  size: 100.0,
-                ),
-                Icon(
-                  Icons.beach_access,
-                  color: Colors.yellow,
-                  size: 100.0,
-                )
-              ],
-            )));
+          appBar: AppBar(
+            title: Text('Ola mundo')
+          ),
+          body: Column(
+            children: <Widget>[
+              Text('Pergunta'),
+              RaisedButton(
+                child: Text("Pergunta 1"),
+                onPressed: null
+              ),
+              RaisedButton(
+                child: Text("Botao 1"),
+                onPressed: null
+              ),
+              RaisedButton(
+                child: Text("Botao 1"),
+                onPressed: null
+              )
+            ]
+            )
+        )
+      );
   }
 }
